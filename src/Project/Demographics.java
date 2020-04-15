d6091ee992392fd2796842e011139c4c22153b1b
ChristianKsOh <57160317+ChristianKsOh@users.noreply.github.com>
2020-04-15 12:21:11 -0700

    Add files via upload

---
+++ src/Project/Demographics.java
@@ -0,0 +1,17 @@
+package Project;
+public class Demographics {
+	private String occupation;
+	private String workStatus;
+	private String educationalDegree;
+	private String notes;
+	
+	/**
+	 * Updates the demographics for the patient
+	 * @param
+	 */
+	public void updateDemographics()
+	{
+		
+	}
+	
+}
---
+++ src/Project/Patient.java
@@ -0,0 +1,24 @@
+package Project;
+
+public class Patient {
+
+	private String name;
+	private String registrationDate;
+	private String gender;
+	private String dateOfBrith;
+	private String phoneNumber;
+	private String socialID;
+	private String insuranceID;
+	private String orderNumber;
+	private Category categoryNum;
+	private int visitNumber;
+	
+	/**
+	 * Updates a patient's profile
+	 * @param name, date, gender, birth, number, social, insurance, category, visit
+	 */
+	public void updateProfile()
+	{
+		
+	}
+}

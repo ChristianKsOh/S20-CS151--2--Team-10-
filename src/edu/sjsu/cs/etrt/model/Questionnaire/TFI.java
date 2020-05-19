f94e26d3ab5ee47c23f19a5a0ce4547cc999b3f7
newbitt <brianla7755@gmail.com>
2020-05-18 20:13:31 -0700

    removed makeForm() & makeAnswer()

--- src/edu/sjsu/cs/etrt/model/Questionnaire/TFI.java
+++ src/edu/sjsu/cs/etrt/model/Questionnaire/TFI.java
@@ -1,6 +1,5 @@
 package edu.sjsu.cs.etrt.model.Questionnaire;
 import edu.sjsu.cs.etrt.model.Patient.*;
-import edu.sjsu.cs.etrt.model.Project.*;
 import javax.swing.JPanel;
 
 
@@ -10,45 +9,6 @@ public class TFI extends Form {
 		super(patient, questionList, answerList, score );
 	}
 	
-	
-	/**
-	 * set up answers for THI
-	 */
-	public void makeAnswers() {
-		for(int i=0;i<11;i++)
-			setAnswer(i, ""+i);		//lazy casting...
-	}
-	
-	public void makeForm(){
-		setQuestion(0, "What percentage of your time were you consciously AWARE OF your tinnitus?");
-		setQuestion(1, "How STRONG or LOUD was your tinnitus?");
-		setQuestion(2, "What percentage of your time awake were you ANNOYED by your tinnitus?");
-		setQuestion(3, "Did you feel IN CONTROL in regards to your tinnitus?");
-		setQuestion(4, "How easy was it for you to COPE with your tinnitus?");
-		setQuestion(5, "How easy was it for you to IGNORE your tinnitus?");
-		setQuestion(6, "Your ability to CONCENTRATE?");
-		setQuestion(7, "Your ability to THINK CLEARLY?");
-		setQuestion(8, "Your ability to FOCUS ATTENTION on other things besides your tinnitus?");
-		setQuestion(9, "How often did your tinnitus make it difficult to FALL ASLEEP or STAY ASLEEP?");
-		setQuestion(10, "How often did your tinnitus cause you difficulty in getting AS MUCH SLEEP"
-				+ "as you needed?");
-		setQuestion(11, "How much of the time did your tinnitus keep you from SLEEPING as DEEPLY"
-				+ "or as PEACEFULLY as you would have liked?");
-		setQuestion(12, "Your ability to HEAR CLEARLY?");
-		setQuestion(13, "Your ability to UUNDERSTAND PEOPLE who are talking?");
-		setQuestion(14, "Your ability to FOLLOW CONVERSATIONS in a group or at meetings?");
-		setQuestion(15, "Your QUIET RESTING ACTIVITIES?");
-		setQuestion(16, "Your ability to RELAX?");
-		setQuestion(17, "Your ability to enjoy 'PEACE AND QUIET'?");
-		setQuestion(18, "Your enjoyment of SOCIAL ACTIVITIES?");
-		setQuestion(19, "Your ENJOYMENT OF LIFE?");
-		setQuestion(20, "Your RELATIONSHIPS with family, friends, and other people?");
-		setQuestion(21, "How often did you tinnitus cause you to have difficulty performing your"
-				+ "WORK OR OTHER TASKS?");
-		setQuestion(22, "How ANXIOUS or WORRIED has your tinnitus made you feel?");
-		setQuestion(23, "How BOTHERED or UPSET have you been because of your tinnitus?");
-		setQuestion(24, "How DEPRESSED were you because of your tinnitus?");
-	}
 	
 	@Override
 	public boolean THI() {
--- src/edu/sjsu/cs/etrt/model/Questionnaire/THI.java
+++ src/edu/sjsu/cs/etrt/model/Questionnaire/THI.java
@@ -1,53 +1,10 @@
 package edu.sjsu.cs.etrt.model.Questionnaire;
 import edu.sjsu.cs.etrt.model.Patient.*;
-import edu.sjsu.cs.etrt.model.Project.*;
 
 public class THI extends Form{
 	
 	public THI (Patient patient, String[] questionList, String[] answerList, int score) {
 		super(patient, questionList, answerList, score);
-	}
-	
-	/**
-	 * set up answers for THI
-	 */
-	public void makeAnswers() {
-		setAnswer(0, "Yes(4)");
-		setAnswer(1, "Sometimes(2)");
-		setAnswer(2, "No (0)");
-	}
-	
-	/**
-	 * set up questions on THI form
-	 */
-	public void makeForm(){
-		setQuestion(0, "Because of your tinnitus, is it difficult for you to concentrate?");
-		setQuestion(1, "Does the loudness of your tinnitus make it difficult for you to hear people?");
-		setQuestion(2, "Does your tinnitus make you angry?");
-		setQuestion(3, "Does your tinnitus make you feel confused?");
-		setQuestion(4, "Because of your tinnitus, do you feel desperate?");
-		setQuestion(5, "Do you complain a great deal about your tinnitus?");
-		setQuestion(6, "Because of your tinnitus, do you have trouble falling to sleep at night?");
-		setQuestion(7, "Do you feel as though you cannot escape your tinnitus?");
-		setQuestion(8, "Does your tinnitus interefere with your ability to enjoy social activities?");
-		setQuestion(9, "Because of your tinnitus, do you feel frustrated?");
-		setQuestion(10, "Because of your tinnitus, do you feel that you have a terrible disease?");
-		setQuestion(11, "Does your tinnitus make it difficult for you to enjoy life?");
-		setQuestion(12, "Does your tinnitus interfere with your job or household responsibilities?");
-		setQuestion(13, "Because of your tinnitus, do you find that you are often irritable?");
-		setQuestion(14, "Because of your tinnitus, is it difficult for you to read?");
-		setQuestion(15, "Does your tinnitus make you upset?");
-		setQuestion(16, "Do you feel that your tinnitus problem has placed more stress on your " 
-		+ "relationships with members of your family and friends?");
-		setQuestion(17, "Do you find it difficult to focus your attention away from your "
-		+ "tinnitus and on other things?");
-		setQuestion(18, "Do you feel that you have no control over your tinnitus?");
-		setQuestion(19, "Because of your tinnitus, do you often feel tired?");
-		setQuestion(20, "Because of your tinnitus, do you feel depressed?");
-		setQuestion(21, "Does your tinnitus make you feel anxious?");
-		setQuestion(22, "Do you feel that you can no longer cope with tinnitus?");
-		setQuestion(23, "Does your tinnitus get worse when you are under stress?");
-		setQuestion(24, "Does your tinnitus make you feel insecure?");
 	}
 	
 	
